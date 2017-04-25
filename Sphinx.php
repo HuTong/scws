0d417355428feaed3e1d56d53672d1fc9f150c19
hutong <hutong0521@163.com>
2017-04-25 14:42:44 +0800

    添加sphinx

---
+++ Sphinx.php
@@ -0,0 +1,90 @@
+<?php
+namespace Hutong\Scws;
+/**
+ * sphinx 搜索
+ *
+ * 中文分词的时候结合scws分词搜索
+ */
+class Sphinx
+{
+    private $sphinx;
+    private $host;
+    private $port;
+
+    public function __construct($host = 'localhost', $port = 9312, $keepActive = false)
+    {
+        if(!extension_loaded('sphinx'))
+        {
+            throw new \Exception('sphinx 扩展未加载');
+        }
+
+        $sphinx = SphinxClient();
+        $sphinx->setServer($host, $port);
+        if($keepActive)
+        {
+            if(!$sphinx->open())
+            {
+                throw new \Exception('sphinx 长连接开启失败');
+            }
+        }
+
+        $this->sphinx = $sphinx;
+    }
+
+    public function Query($q, $index, $mode = SPH_MATCH_ALL, $options = array())
+    {
+        $this->sphinx->SetArrayResult();
+        $this->sphinx->SetFieldWeights(array(100,1));
+        $this->sphinx->SetMatchMode($mode);
+
+        if($options)
+        {
+            if((isset($options['filter']) && $options['filter']) && ($options['filtervals']) && $options['filtervals']))
+            {
+                $this->sphinx->SetFilter($options['filter'], $options['filtervals']);
+            }
+            if((isset($options['groupby']) && $options['groupby']) && ($options['groupsort']) && $options['groupsort']))
+            {
+                $this->sphinx->SetGroupBy($options['groupby'], SPH_GROUPBY_ATTR, $options['groupsort']);
+            }
+            if(isset($options['sortby']) && $options['sortby'])
+            {
+                $this->sphinx->SetSortMode(SPH_SORT_EXTENDED, $options['sortby']);
+            }
+            if(isset($options['sortexpr']) && $options['sortexpr'])
+            {
+                $this->sphinx->SetSortMode(SPH_SORT_EXPR, $options['sortexpr']);
+            }
+            if(isset($options['distinct']) && $options['distinct'])
+            {
+                $this->sphinx->SetGroupDistinct($options['distinct']);
+            }
+            if(isset($options['select']) && $options['select'])
+            {
+                $this->sphinx->SetSelect($options['select']);
+            }
+            $limit = isset($options['limit']) ? (int)$options['limit'] ? 1000;
+            if($limit)
+            {
+                $this->sphinx->SetLimits(0, $limit, ($limit>1000) ? $limit : 1000);
+            }
+            if(isset($options['ranker']) && $options['ranker'])
+            {
+                $this->sphinx->SetRankingMode($options['ranker']);
+            }
+            if(isset($options['resArray']) && $options['resArray'])
+            {
+                $this->sphinx->SetArrayResult(true);
+            }
+        }
+
+        $res = $this->sphinx->query($q, $index);
+
+        if($res == false)
+        {
+            return false;
+        }
+
+        return $res;
+    }
+}
